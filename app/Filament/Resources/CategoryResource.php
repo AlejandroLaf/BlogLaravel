3f840cc3db5b4e804ca5a9963ff5f086750b36a7
Alejandro Lafarga <alafargagatius@iesmordefuentes.com>
2024-01-17 11:02:06 +0100

    Mejorando el panel de categorias del admin (12:50 video 9)

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -10,9 +10,11 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class CategoryResource extends Resource
 {
@@ -24,8 +26,16 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('title')->required()->maxLength(150),
-                TextInput::make('slug')->required()->unique(ignoreRecord:true),
+                TextInput::make('title')
+                ->live()
+                ->required()->minLength(1)->maxLength(150)
+                ->afterStateUpdated(function(string $operation, $state, Forms\Set $set) {
+                    if($operation === 'edit'){
+                        return;
+                    }
+                    $set('slug',Str::slug($state));
+                }),
+                TextInput::make('slug')->required()->minLength(1)->unique(ignoreRecord:true),
                 TextInput::make('text_color')->nullable(),
                 TextInput::make('bg_color')->nullable(),
             ]);
@@ -35,7 +45,10 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('slug')->sortable()->searchable(),
+                TextColumn::make('text_color')->sortable()->searchable(),
+                TextColumn::make('bg_color')->sortable()->searchable(),
             ])
             ->filters([
                 //
