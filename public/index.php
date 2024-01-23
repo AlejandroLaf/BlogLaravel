1d6996e1907c9f582c68704c9208863a001e77d1
Alejandro Lafarga <alafargagatius@iesmordefuentes.com>
2024-01-23 13:55:29 +0100

    Intentando arreglar algo

--- public/index.php
+++ public/index.php
@@ -3,6 +3,14 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Max-Age: 86400');
+    exit;
+}
+
 define('LARAVEL_START', microtime(true));
 
 /*
