3e5d3f022bd92d0cd174e86f952cb0fc1dbaa4bf
Alex <alexlafarga2001@gmail.com>
2024-01-30 18:42:10 +0100

    Cacheo de la pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
 
     <div class="mb-10 w-full">
         <div class="mb-16">
-            <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
+            <h2 class="mt-16 mb-5 text-3xl text-red-500 font-bold">Featured Posts</h2>
             <div class="w-full">
                 <div class="grid grid-cols-3 gap-10 w-full">
                     @foreach ($featuredPosts as $post)
@@ -21,13 +21,13 @@
                     @endforeach
                 </div>
             </div>
-            <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
+            <a class="mt-10 block text-center text-lg text-red-500 font-semibold"
                 href="http://127.0.0.1:8000/blog">More
                 Posts</a>
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Latest Posts</h2>
+        <h2 class="mt-16 mb-5 text-3xl text-red-500 font-bold">Latest Posts</h2>
         <div class="w-full mb-5">
             <div class="grid grid-cols-3 gap-10 w-full">
                 @foreach ($latestPosts as $post)
@@ -35,7 +35,7 @@
                 @endforeach
             </div>
         </div>
-        <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold" href="http://127.0.0.1:8000/blog">More
+        <a class="mt-10 block text-center text-lg text-red-500 font-semibold" href="http://127.0.0.1:8000/blog">More
             Posts</a>
     </div>
 </x-app-layout>
