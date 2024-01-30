c2fd426fedc70ef7845c258eae0a2774839d43f7
Alex <alexlafarga2001@gmail.com>
2024-01-30 20:37:01 +0100

    Traduccion pagina principal

---
+++ lang/en/menu.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return [
+
+    'home' => 'Home',
+    'blog' => 'Blog',
+    'admin' => 'Admin',
+
+    'manage_account' => "Manage Account",
+    'profile' => 'Profile',
+    'logout' => 'Log Out',
+    'login' => 'Login',
+    'register' => 'Register'
+
+
+];
